76de8a807b6fe3a7fa8dce14f0dc899d3fd4e4ef
thinhdabezt <thinhdinhtran0605@gmail.com>
2025-06-18 10:36:33 +0700

    Add onclick for all buttons on menu and how to play scene

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OpenHowToPlayScene()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
