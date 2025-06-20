004ef43bf8fdecb48925ef15ce2188a220310ed4
thinhdabezt <thinhdinhtran0605@gmail.com>
2025-06-20 19:14:53 +0700

    Add new animals: boar. Modify level3 tilemap

---
+++ Assets/Scripts/HealthPickup.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class HealthPickup : MonoBehaviour
+{
+    public int healthAmount = 20;
+    public Vector3 spinRotationSpeed = new Vector3(0, 180, 0);
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.eulerAngles += spinRotationSpeed * Time.deltaTime;
+    }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    Damageable damageable = collision.GetComponent<Damageable>();
+
+    //    if (damageable)
+    //    {
+    //        bool wasHealed = damageable.Heal(healthAmount);
+    //        if (wasHealed)
+    //        {
+    //            if (audioSource)
+    //            {
+    //                AudioSource.PlayClipAtPoint(audioSource.clip, gameObject.transform.position, audioSource.volume);
+    //            }
+    //            Destroy(gameObject);
+    //        }
+    //    }
+    //}
+}
